6c3538cdcec30eefd8d38b464cdf7b993c32ec67
hasculdr <hasculdr@mail.ru>
2019-10-19 18:30:42 +0400

    +9.1a

--- exercises/09_functions/task_9_1a.py
+++ exercises/09_functions/task_9_1a.py
@@ -40,5 +40,29 @@ access_config = {
     'FastEthernet0/16': 17
 }
 
+def generate_access_config(intf_vlan_mapping, access_template, psecurity=None):
+	'''
+	intf_vlan_mapping - словарь с соответствием интерфейс-VLAN такого вида:
+		{'FastEthernet0/12':10,
+		'FastEthernet0/14':11,
+		'FastEthernet0/16':17}
+	access_template - список команд для порта в режиме access
+
+	Возвращает список всех портов в режиме access с конфигурацией на основе шаблона
+	'''
+	output_list = []
+	for elem in intf_vlan_mapping:
+		output_list.append(f'interface {elem}')
+		for command in access_template:
+			if command.endswith('vlan'):
+				output_list.append(f'{command} {access_config[elem]}')
+			else:
+				output_list.append(f'{command}')
+		if psecurity:
+			for command in psecurity:
+				output_list.append(f'{command}')
+	return(output_list)
+print(generate_access_config(access_config, access_mode_template, port_security_template))
+
 
 
