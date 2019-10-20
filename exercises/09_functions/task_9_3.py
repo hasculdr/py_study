6785a353e1729ce12facb43bb497751deba1eb30
hasculdr <hasculdr@mail.ru>
2019-10-20 21:16:37 +0400

    +9.3

--- exercises/09_functions/task_9_3.py
+++ exercises/09_functions/task_9_3.py
@@ -22,3 +22,21 @@
 Ограничение: Все задания надо выполнять используя только пройденные темы.
 '''
 
+def get_int_vlan_map(config_filename):
+	access_dict = {}
+	trunk_dict = {}
+	data = (open(config_filename, 'r')).readlines()
+	for line in data:
+		if line.startswith('interface '):
+			temp = (line.strip())[10:]
+			continue
+		elif line.startswith(' switchport access vlan'):
+			access_dict[temp] = int(line[23:-1])
+		elif line.startswith(' switchport trunk allowed vlan'):
+			allowed_list_str = (line[31:-1]).split(',')
+			allowed_list_int = []
+			for elem in allowed_list_str:
+				allowed_list_int.append(int(elem))
+			trunk_dict[temp] = allowed_list_int
+	return(access_dict, trunk_dict)
+print(get_int_vlan_map('config_sw1.txt'))
