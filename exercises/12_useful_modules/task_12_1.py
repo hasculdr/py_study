35a4a68290555e39a533baf165cb0813b500fbe9
hasculdr <hasculdr@mail.ru>
2019-11-17 16:40:37 +0400

    +12.1

--- exercises/12_useful_modules/task_12_1.py
+++ exercises/12_useful_modules/task_12_1.py
@@ -14,4 +14,18 @@
 
 Ограничение: Все задания надо выполнять используя только пройденные темы.
 '''
-
+import subprocess
+list_addresses = ['8.8.8.8', '88.147.158.9', '10.10.10.10', '192.168.0.4', '0.0.0.0']
+def ping_ip_addresses(list_addresses):
+	alive = list()
+	unreachable = list()
+	for address in list_addresses:
+		reply = subprocess.run(['ping', '-c', '1', address], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+		if reply.returncode == 0:
+			alive.append(address)
+		else:
+			unreachable.append(address)
+	output = (alive, unreachable)
+	return(output)
+if __name__ == "__main__":
+	print(ping_ip_addresses(list_addresses))
