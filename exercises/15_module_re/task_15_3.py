a4411b06ff9f3d44d940a51eb661dfc57ed954e3
hasculdr <hasculdr@mail.ru>
2019-12-08 21:41:48 +0400

    +15.3

--- exercises/15_module_re/task_15_3.py
+++ exercises/15_module_re/task_15_3.py
@@ -31,4 +31,17 @@ object network LOCAL_10.1.9.5
 
 Во всех правилах для ASA интерфейсы будут одинаковыми (inside,outside).
 '''
-
+import re
+def convert_ios_nat_to_asa(nat, asa):
+	#			  nat  inside       static tcp			ip			 port1         port2
+	regex = r'ip (\w+) (\w+) source (\w+) (\w+) (\d+\.\d+\.\d+\.\d+) (\d+) \S+ \S+ (\d+)'
+	values = []
+	with open(nat, 'r') as nat_list:
+		ios_rules = nat_list.read()
+	values = (re.findall(regex, ios_rules))
+	with open(asa, 'w') as asa_list:
+		for elem in values:
+			asa_list.write(f'object network LOCAL_{elem[4]}\n host {elem[4]}\n {elem[0]} (inside,outside) {elem[2]} interface service {elem[3]} {elem[5]} {elem[6]}\n')
+print(convert_ios_nat_to_asa('cisco_nat_config.txt', '15_3_output.txt'))	
+	
+	
