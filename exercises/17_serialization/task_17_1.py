035352c6f44fabdddced741358a541d2e80e12fc
hasculdr <hasculdr@mail.ru>
2019-12-29 21:58:09 +0400

    +17.1

--- exercises/17_serialization/task_17_1.py
+++ exercises/17_serialization/task_17_1.py
@@ -40,8 +40,44 @@
 '''
 
 import glob
-
+import re
+import csv
 sh_version_files = glob.glob('sh_vers*')
 #print(sh_version_files)
 
 headers = ['hostname', 'ios', 'image', 'uptime']
+data_filenames = sh_version_files
+csv_filename = 'routers_inventory.csv'
+
+def parse_sh_version(show_version_string):
+	ios_r = r'Cisco IOS Software.+?Version (\S+),'
+	uptime_r = r'.+?uptime is (.+?)\n'
+	image_r = r'.+?System image file is "(\S+)"'
+	ios = re.search(ios_r, show_version_string, re.DOTALL).group(1)
+	uptime = re.search(uptime_r, show_version_string, re.DOTALL).group(1)
+	image = re.search(image_r, show_version_string, re.DOTALL).group(1)
+	return(ios, image, uptime)
+
+def write_inventory_to_csv(data_filenames, csv_filename):
+	host_list = []
+	data_tuple_list = []
+	data_list = []
+	index = 0
+	for elem in data_filenames:
+		regex = r'.+_(.+?)\..+'#для определения имени узла
+		match = re.findall(regex, elem)
+		host_list.append(match[0])
+		with open(elem , 'r') as temp:
+			show_version_string = temp.read()
+			data_tuple_list.append(parse_sh_version(show_version_string))#засовываем в список возврат функции
+	for elem in data_tuple_list:#получаем из списка_кортежей список_списков
+		data_list.append(list(elem))
+	for elem in data_list:#добавляем элементам списка_списков имена узлов из другого списка
+		elem.insert(0, host_list[index])
+		index += 1
+	with open(csv_filename, 'w') as temp:
+		writer = csv.writer(temp)
+		writer.writerow(headers)
+		for elem in data_list:
+			writer.writerow(elem)
+print(write_inventory_to_csv(data_filenames, csv_filename))
