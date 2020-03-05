8735b75ba91f8cb243d0145efd4232cf4d0ceaf6
hasculdr <hasculdr@gmail.com>
2020-03-05 14:03:55 +0400

    18.1 add dbcheck

--- exercises/18_db/task_18_1/create_db.py
+++ exercises/18_db/task_18_1/create_db.py
@@ -1,9 +1,28 @@
 # -*- coding: utf-8 -*-
 #!/usr/bin/env python3
+
 import os
-if os.path.exists('dhcp_snooping.db'):
-    print("файл есть")
-    print(os.path.exists('dhcp_snooping.db'))
-else:
-    print("файла нет")
-    print(os.path.exists('dhcp_snooping.db'))
+import sqlite3
+
+def filecheck(dbname):
+    if os.path.exists(dbname):#если БД существует - истина
+        return(True)
+    else:#не существует - ложь
+        return(False)
+
+
+def dbcreate(dbname, schema):
+    with open(schema, 'r') as file:
+        commands = file.read()
+    connection = sqlite3.connect(dbname)#создаем соединение с новой БД
+    connection.executescript(commands)#выполняем команды схемы
+    connection.close()#закрываем соединение с БД
+
+
+
+if __name__ == '__main__':
+    if not filecheck('dhcp_snooping.db'):#если БД не существует (значение проверки - ложь)
+        print('Создаю базу данных...')
+        dbcreate('dhcp_snooping.db', 'dhcp_snooping_schema.sql')
+    else:
+        print('База данных существует')
