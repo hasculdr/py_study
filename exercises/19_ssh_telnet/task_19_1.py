b05e075dbffd95977d651c69ce216b38ccedfa4d
hasculdr <hasculdr@mail.ru>
2020-06-08 00:04:05 +0400

    +19.1

--- exercises/19_ssh_telnet/task_19_1.py
+++ exercises/19_ssh_telnet/task_19_1.py
@@ -15,5 +15,21 @@
 Скрипт должен отправлять команду command на все устройства из файла devices.yaml с помощью функции send_show_command.
 
 '''
+from netmiko import ConnectHandler
+import yaml
 
 command = 'sh ip int br'
+
+def send_show_command(device, command):
+	print(f'Соединяюсь с устройством {device["ip"]}')
+	with ConnectHandler(**device) as ssh_session:
+		ssh_session.enable()
+		return(ssh_session.send_command(command))
+		
+if __name__ == "__main__":
+	with open('devices.yaml', 'r') as f:
+		device_list = yaml.safe_load(f)
+	for device in device_list:
+		print(send_show_command(device, command))
+
+
