a8165d6a33aae52240130e7cf90eadacad15e145
hasculdr <hasculdr@mail.ru>
2020-06-22 15:35:13 +0400

    +21.1

--- exercises/21_jinja2/task_21_1.py
+++ exercises/21_jinja2/task_21_1.py
@@ -13,3 +13,21 @@
 Проверить работу функции на шаблоне templates/for.txt и данных из файла data_files/for.yml.
 
 """
+import yaml
+from jinja2 import Environment, FileSystemLoader
+
+
+def generate_config(template, data_dict):
+	template_loader_args=path.split('/')
+	env=Environment(loader=FileSystemLoader(template_loader_args[0]))
+	template=env.get_template(template_loader_args[1])
+	return(template.render(data_dict))
+
+
+path='templates/for.txt'
+
+
+if __name__=="__main__":
+	with open('data_files/for.yml', 'r') as f:
+		data_dict=yaml.safe_load(f)
+	print(generate_config(path, data_dict))
